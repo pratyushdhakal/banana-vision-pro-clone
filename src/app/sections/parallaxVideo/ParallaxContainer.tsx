48d86d4b084752b3acece1693644db3267377eaa
Pratyush Dhakal <pratyushdhakal.personal@gmail.com>
2025-06-27 13:24:17 +0545

    decrease  parallax container height and scroll transform tweens

--- src/app/sections/parallaxVideo/ParallaxContainer.tsx
+++ src/app/sections/parallaxVideo/ParallaxContainer.tsx
@@ -4,10 +4,10 @@ import ParallaxVideoContainer from "./ParallaxVideoContainer";
 export default function ParallaxContainer({ parallaxItem }: ParallaxItemProps) {
 	return (
 		<section>
-			<div className='h-[250vh] relative'>
+			<div className='h-[220vh] relative'>
 				<ParallaxVideoContainer parallaxItem={parallaxItem} />
 			</div>
-			<div className='py-19 grid gap-5 grid-cols-[40%_60%] px-30'>
+			<div className='py-19 grid gap-20 grid-cols-[40%_60%] px-30'>
 				<div className='text-4xl font-bold'>{parallaxItem.heading}</div>
 				<div className='text-2xl font-medium opacity-60'>
 					{parallaxItem.largeDescription}
--- src/app/sections/parallaxVideo/ParallaxVideoContainer.tsx
+++ src/app/sections/parallaxVideo/ParallaxVideoContainer.tsx
@@ -14,26 +14,26 @@ export default function ParallaxVideoContainer({
 	const targetRef = useRef<HTMLDivElement>(null);
 	const { scrollYProgress } = useScroll({
 		target: targetRef,
-		offset: ["start end", "end start"]
+		offset: ["center end", "end start"]
 	});
+	const opacity = useTransform(
+		scrollYProgress,
+		[0, 0.25, 0.3224, 0.3539],
+		[0, 0, 1, 0],
+		{ clamp: false }
+	);
 
 	useMotionValueEvent(scrollYProgress, "change", latest => {
 		console.log({ latest });
 	});
-
-	const opacity = useTransform(
-		scrollYProgress,
-		[0.1, 0.25, 0.45, 0.5, 0.6],
-		[0, 0, 1, 1, 0]
-	);
-	const scale = useTransform(scrollYProgress, [0.6, 1], [1, 0.85]);
+	const scale = useTransform(scrollYProgress, [0.4, 1], [1, 0.85]);
 
 	return (
 		<>
 			<motion.div
 				style={{ scale }}
 				ref={targetRef}
-				className='h-[calc(100dvh-var(--secondary-nav-height))] w-full sticky top-[var(--secondary-nav-height)]'
+				className='h-[calc(100vh-var(--secondary-nav-height))] w-full sticky top-[var(--secondary-nav-height)]'
 			>
 				<video
 					autoPlay
@@ -44,10 +44,10 @@ export default function ParallaxVideoContainer({
 				/>
 				<motion.div
 					style={{ opacity }}
-					className='w-full h-full hello bg-black/70 absolute inset-0 z-850'
+					className='w-full h-full  bg-black/70 absolute inset-0 z-850'
 				/>
 			</motion.div>
-			<Wrapper className='relative  z-750 text-white text-5xl text-center'>
+			<Wrapper className='absolute left-1/2  w-[100%] -translate-x-1/2  z-750 text-white text-5xl text-center'>
 				<p>{parallaxItem.text}</p>
 			</Wrapper>
 		</>
